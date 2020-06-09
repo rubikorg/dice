988ba05c6c1fe7242cff433903d770e6188ca37a
codekid <ckrackerx15@gmail.com>
2020-06-09 11:44:58 +0530

    fix connections and decide types

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/rubikorg/dice
 
 go 1.14
 
-require github.com/lib/pq v1.6.0
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/lib/pq v1.6.0
+	github.com/pelletier/go-toml v1.4.0
+	github.com/rogpeppe/go-internal v1.3.0
+	go.mongodb.org/mongo-driver v1.3.4
+	go.uber.org/zap v1.15.0
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa
+)
--- postgres/open.go
+++ postgres/open.go
@@ -5,20 +5,27 @@ import (
 	"fmt"
 
 	// this import is is used to only load the driver needed
-	// if main package has it it will have to import all the
-	// dialects driver
+	// if main package has it it will have side effects of
+	// all the dialects driver
 	_ "github.com/lib/pq"
 	"github.com/rubikorg/dice"
 )
 
 // Connect to postgresql db using the connection uri
 func Connect(uri dice.ConnectUri) (*sql.DB, error) {
-	fmtStr := "host=%s port=%d user=%s password=%s dbname=%s "
+	var connStr = ""
+	connStr += "host=" + uri.Host
+	connStr += fmt.Sprintf(" port=%d", uri.Port)
+	connStr += " user=" + uri.Username
+	connStr += " dbname=" + uri.Database
+
 	if !uri.SSL {
-		fmtStr += "sslmode=disable"
+		connStr += " sslmode=disable"
 	}
 
-	cstr := fmt.Sprintf(fmtStr,
-		uri.Host, uri.Port, uri.Username, uri.Password, uri.Database)
-	return sql.Open("postgres", cstr)
+	if uri.Password != "" {
+		connStr += " password=" + uri.Password
+	}
+
+	return sql.Open(string(dice.Postgres), connStr)
 }
